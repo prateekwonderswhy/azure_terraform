cafc27e77cf6981b13a5ec254bac3536e506ea58
prateekwonderswhy <gargprateek1998@protonmail.com>
2020-06-24 15:16:10 +0530

    Deleted terraform.tf

--- terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend  "azurerm" {
-        resource_group_name = "remote-state"
-        storage_account_name = "prateekremotestate"
-        container_name = "tfstates"
-        key = "web.tfstate"
-    }
-}
